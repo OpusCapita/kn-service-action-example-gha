41bf3cf64d20bc1bc8f1071e3374275694a98113
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-11-26 13:11:10 +0530

    Repo Init

---
+++ src/main/java/dev/kameshs/FruitResource.java
@@ -0,0 +1,17 @@
+package dev.kameshs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public class FruitResource {
+
+    @GET
+    @Path("/fruit")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String fruit() {
+        return "apple";
+    }
+}
