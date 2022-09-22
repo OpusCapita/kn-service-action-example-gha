097c85cce8c5c5b816f45fa580475eb2d846b10d
Simon Spiridonov <doom@serverland.ru>
2022-09-22 14:24:37 +0200

    init

--- src/main/java/dev/kameshs/HttpMocking.java
+++ src/main/java/dev/kameshs/HttpMocking.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 /**
  * Azure Functions with HTTP Trigger.
  */
+@Path("/oc")
 public class HttpMocking {
 
     static Logger logger = Logger.getLogger(HttpMocking.class.getName());
